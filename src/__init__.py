4554a16b2311ff93b69bc3abb686fdfac8877677
Абылай Слямжанов <abylayslamzhanov@MacBook-Air-Abylaj.local>
2025-09-10 21:41:53 +0500

    Update src package info

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,9 @@
 """
 Face Detection Project - Source Code
+Author: Abylay Slamzhanov
+GitHub: https://github.com/abylsliam44/protectorai-test
 """
 
 __version__ = "1.0.0"
 __author__ = "Abylay Slamzhanov"
+__github__ = "https://github.com/abylsliam44/protectorai-test"
